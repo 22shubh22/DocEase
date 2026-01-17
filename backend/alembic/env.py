594dfe19a53f099e5f652d01abab79397212e175
Shubham Goyal <shubham.g@cognavi.com>
2026-01-17 16:11:25 +0530

    some alembic files

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -3,8 +3,11 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from dotenv import load_dotenv
+load_dotenv()
 
 from alembic import context
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
